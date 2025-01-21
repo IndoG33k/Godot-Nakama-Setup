1edd4c97aa8d38849200e3f3d7f4b46be687d596
IndoG33k <muhammadabdullahmamoon@gmail.com>
2025-01-21 20:49:50 +0500

    Sample Pong Game
    Successfully integrated Godot Nakama functionality with a sample pong game to sync across game data and state.
    
    Parent Branch: main
    Pushed By: Abdullah Mamoon
    Date: 21-Jan-2025
    Time: 8:51pm

--- Player.gd
+++ Player.gd
@@ -1,29 +1,43 @@
 extends CharacterBody2D
 
-
+   #Does this belong here ??? Will the instantiation be an issue ?
+const PADDLE_SPEED : int = 500
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var win_height : int
+var p_height : int
+
+func _ready() -> void:
+	win_height = get_viewport_rect().size.y
+	p_height = $ColorRect.get_size().y
+	pass
 
 func _physics_process(delta: float) -> void:
 	if name == str(multiplayer.get_unique_id()):
+		if Input.is_action_pressed("ui_up"):
+			position.y -= PADDLE_SPEED * delta
+		elif Input.is_action_pressed("ui_down"):
+			position.y += PADDLE_SPEED * delta
+		
+		position.y = clamp(position.y, p_height / 2, win_height - p_height / 2)
 		# Add the gravity.
-		if not is_on_floor():
-			velocity += get_gravity() * delta
-
-		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis("ui_left", "ui_right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-
-		move_and_slide()
+		#if not is_on_floor():
+			#velocity += get_gravity() * delta
+#
+		## Handle jump.
+		#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			#velocity.y = JUMP_VELOCITY
+#
+		## Get the input direction and handle the movement/deceleration.
+		## As good practice, you should replace UI actions with custom gameplay actions.
+		#var direction := Input.get_axis("ui_left", "ui_right")
+		#if direction:
+			#velocity.x = direction * SPEED
+		#else:
+			#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+		#move_and_slide()
 		syncPos.rpc(global_position)
 
 @rpc("any_peer")
--- SceneManager.gd
+++ SceneManager.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var score := [0, 0]
 var spawnpoints
 @export var playerScene : PackedScene
 # Called when the node enters the scene tree for the first time.
@@ -20,6 +21,16 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_ball_timer_timeout() -> void:
+	$Ball.new_ball()
+
+
+func _on_score_left_body_entered(body):
+	score[1] += 1
+	$HUD/PlayerScore2.text = str(score[1])
+	$BallTimer.start()
+	
+func _on_score_right_body_entered(body):
+	score[0] += 1
+	$HUD/PlayerScore.text = str(score[0])
+	$BallTimer.start()
